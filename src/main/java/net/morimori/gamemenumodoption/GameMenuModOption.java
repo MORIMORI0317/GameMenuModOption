493b966aa862b58372f4e97369fec1682bdcd92a
MORIMORI <morimori.0317@outlook.jp>
2020-06-27 21:13:46 +0900

    1.16.1

--- src/main/java/net/morimori/gamemenumodoption/GameMenuModOption.java
+++ src/main/java/net/morimori/gamemenumodoption/GameMenuModOption.java
@@ -5,6 +5,7 @@ import net.minecraft.client.gui.screen.IngameMenuScreen;
 import net.minecraft.client.gui.widget.Widget;
 import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.client.resources.I18n;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -14,36 +15,35 @@ import net.minecraftforge.fml.common.Mod;
 @Mod("gamemenumodoption")
 public class GameMenuModOption {
 
-	public GameMenuModOption() {
-		MinecraftForge.EVENT_BUS.register(this);
-	}
+    public GameMenuModOption() {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
 
-	@SubscribeEvent
-	public void onGUI(GuiScreenEvent.InitGuiEvent.Post e) {
+    @SubscribeEvent
+    public void onGUI(GuiScreenEvent.InitGuiEvent.Post e) {
 
-		if (e.getGui() instanceof IngameMenuScreen) {
+        if (e.getGui() instanceof IngameMenuScreen) {
 
-			e.addWidget(new Button(e.getWidgetList().get(6).x, e.getWidgetList().get(6).y + 24,
-					e.getWidgetList().get(6).getWidth(), e.getWidgetList().get(6).getHeight(),
-					I18n.format("menu.modoption"),
-					(p_213055_1_) -> {
-						Minecraft.getInstance()
-								.displayGuiScreen(new ModListScreen(e.getGui()));
+            e.addWidget(new Button(e.getWidgetList().get(6).field_230690_l_, e.getWidgetList().get(6).field_230691_m_ + 24,
+                    e.getWidgetList().get(6).func_230998_h_(), e.getWidgetList().get(6).getHeight(),
+                    new StringTextComponent(I18n.format("menu.modoption")),
+                    (p_213055_1_) -> {
+                        Minecraft.getInstance()
+                                .displayGuiScreen(new ModListScreen(e.getGui()));
 
-					}));
+                    }));
 
-			e.getWidgetList().get(6).x = e.getGui().width / 2 - 102;
-			e.getWidgetList().get(6).setWidth(204);
+            e.getWidgetList().get(6).field_230690_l_ = e.getGui().field_230708_k_ / 2 - 102;
+            e.getWidgetList().get(6).func_230991_b_(204);
 
-			e.getWidgetList().get(7).y += 24;
+            e.getWidgetList().get(7).field_230691_m_ += 24;
 
-			e.getWidgetList().get(5).y += 24;
+            e.getWidgetList().get(5).field_230691_m_ += 24;
 
-			for (Widget in : e.getWidgetList()) {
-				in.y -= 16;
-			}
+            for (Widget in : e.getWidgetList()) {
+                in.field_230691_m_ -= 16;
+            }
+        }
 
-		}
-
-	}
+    }
 }
