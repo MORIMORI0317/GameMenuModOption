bf3e5952984d5dfd24af58922f49469c63028cbf
MORIMORI0317 <morimori.0317@outlook.jp>
2021-09-07 05:47:28 +0900

    1.9

--- README.md
+++
@@ -1,2 +0,0 @@
-# GameMenuModOption
-Added MOD settings to the game menu as before 1.12.2 
--- src/main/java/net/morimori/gamemenumodoption/GameMenuModOption.java
+++ src/main/java/net/morimori/gamemenumodoption/GameMenuModOption.java
@@ -1,10 +1,19 @@
 package net.morimori.gamemenumodoption;
 
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod("gamemenumodoption")
+@Mod(GameMenuModOption.MODID)
 public class GameMenuModOption {
+    public static final String MODID = "gamemenumodoption";
+
     public GameMenuModOption() {
-        ClientConfig.init();
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+        MinecraftForge.EVENT_BUS.register(GuiHandler.class);
     }
 }
