84d83cfee053b85d9a4f27333f42c9214393755d
MORIMORI0317 <morimori.0317@outlook.jp>
2021-09-24 15:22:47 +0900

    mixin

--- src/main/java/net/morimori/gamemenumodoption/GameMenuModOption.java
+++ src/main/java/net/morimori/gamemenumodoption/GameMenuModOption.java
@@ -1,19 +1,12 @@
 package net.morimori.gamemenumodoption;
 
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(GameMenuModOption.MODID)
 public class GameMenuModOption {
     public static final String MODID = "gamemenumodoption";
 
     public GameMenuModOption() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-    }
-
-    private void doClientStuff(final FMLClientSetupEvent event) {
-        MinecraftForge.EVENT_BUS.register(GuiHandler.class);
+        ClientConfig.init();
     }
 }
---
+++ src/main/java/net/morimori/gamemenumodoption/mixin/ScreenMixin.java
@@ -0,0 +1,28 @@
+package net.morimori.gamemenumodoption.mixin;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fmlclient.gui.screen.ModListScreen;
+import net.morimori.gamemenumodoption.ClientConfig;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Screen.class)
+public abstract class ScreenMixin {
+    @Shadow
+    public abstract void renderDirtBackground(int p_96627_);
+
+    @Inject(method = "renderBackground(Lcom/mojang/blaze3d/vertex/PoseStack;I)V", at = @At("TAIL"), cancellable = true)
+    private void renderBackground(PoseStack p_96559_, int vOffset, CallbackInfo ci) {
+        if (ModList.get().isLoaded("bettergamemenu")) return;
+        Screen screen = (Screen) (Object) this;
+        if (screen instanceof ModListScreen && ClientConfig.RenderModListBackground.get()) {
+            ci.cancel();
+            this.renderDirtBackground(vOffset);
+        }
+    }
+}
