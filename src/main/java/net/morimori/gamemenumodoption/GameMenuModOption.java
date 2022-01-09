aa4b2b021ec8323033c7b0658cced83c18c7b38d
MORIMORI0317 <morimori.0317@outlook.jp>
2022-01-09 20:24:37 +0900

    fix server list FML incompatibility notification

--- src/main/java/net/morimori/gamemenumodoption/GameMenuModOption.java
+++ src/main/java/net/morimori/gamemenumodoption/GameMenuModOption.java
@@ -1,10 +1,15 @@
 package net.morimori.gamemenumodoption;
 
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Mod("gamemenumodoption")
 public class GameMenuModOption {
     public GameMenuModOption() {
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         ClientConfig.init();
     }
 }
